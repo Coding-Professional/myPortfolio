17e9726646c7b5de8b794f7938ccad6c326205ef
Satyam Yadav <satyam834996@gmail.com>
2024-11-25 13:34:56 +0530

    new ui and files

--- src/info/Info.js
+++ src/info/Info.js
@@ -8,7 +8,7 @@ import mock5 from "../img/mock5.png"
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
 
-export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+export let colors = ["rgb(255 200 0)", "rgb(166,104,255)"];
 /*
 I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
 These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
@@ -27,29 +27,36 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    firstName: "Satyam",
+    lastName: "Yadav",
+    initials: "js",
     position: "a Full Stack Developer",
-    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    selfPortrait: self, 
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, 
     baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    miniBio: [
         {
             emoji: '☕',
-            text: 'fueled by coffee'
+            text: 'founder of ProCoderJii Youtube'
         },
         {
             emoji: '🌎',
-            text: 'based in the US'
-        },
-        {
-            emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: 'based in the Bhopal'
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: " let's get in touch!"
+        },
+        {
+            emoji: "📝",
+            text: (
+                <>
+                     Download my <a href="/SatyamYadavResume.pdf" target="_blank" style={{
+                        color: 'blue', 
+                        textDecoration: 'underline', 
+                    }} rel="noopener noreferrer" download>resume</a>
+                </>
+            )
         }
     ],
     socials: [
@@ -78,16 +85,15 @@ export const info = {
             icon: "fa fa-twitter",
             label: 'twitter'
         }
-// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-// Just change the links so that they lead to your social profiles.
+
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
-    skills:
-        {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
-        }
+    bio: "Hello! I'm Satyam, a systems engineer specializing in ASP.NET, AI, and ML, with a degree in Artificial Intelligence and Machine Learning. I'm passionate about leveraging technology to drive innovation. Let's build the future together! 😊",
+   skills: {
+    proficientWith: ['javascript', 'angularjs', 'asp.net core', 'c#', 'mssql', 'bootstrap', 'html5', 'css3', 'tailwindcss'],
+    exposedTo: ['flutter', 'dart', 'machine learning', 'Canva']
+}
+
     ,
     hobbies: [
         {
@@ -95,25 +101,20 @@ export const info = {
             emoji: '📖'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
+            label: 'Travel',
+            emoji: '😍'
         },
         {
-            label: 'movies',
-            emoji: '🎥'
-        },
-        {
-            label: 'cooking',
-            emoji: '🌶'
+            label: 'Bussiness',
+            emoji: '💰'
         }
-// Same as above, change the emojis to match / relate to your hobbies or interests.
-// You can also remove the emojis if you'd like, I just think they look cute :P
+
     ],
-    portfolio: [ // This is where your portfolio projects will be detailed
+    portfolio: [ 
         {
             title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+            live: "https://paytonpierce.dev", 
+            source: "https://github.com/paytonjewell",
             image: mock1
         },
         {
