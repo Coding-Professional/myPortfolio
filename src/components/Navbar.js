61e6dcc6335654e5f6a1db96c8e72fa9ecc9ad44
Satyam Yadav <satyam834996@gmail.com>
2025-04-26 23:55:58 +0530

    new changes and updating ui functions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,22 @@
+// Uncomment the following sections if you want to include Home, About Me, and Portfolio links in the future:
+
+// {
+//     name: 'Home',
+//     to: '',
+//     active: 'home'
+// },
+// {
+//     name: 'About Me',
+//     to: 'about',
+//     active: 'about'
+// },
+// {
+//     name: 'Portfolio',
+//     to: 'portfolio',
+//     active: 'portfolio'
+// }
+
+
 import React, { useState } from 'react';
 import Style from './Navbar.module.scss';
 import Toggler from "./home/Toggler";
@@ -8,42 +27,43 @@ import { info } from "../info/Info";
 import { singlePage } from '../info/Info';
 
 const links = [
+    // {
+    //     name: 'Home',
+    //     to: '',
+    //     active: 'home'
+    // },
+    // {
+    //     name: 'About Me',
+    //     to: 'about',
+    //     active: 'about'
+    // },
     {
-        name: 'Home',
-        to: '',
-        active: 'home'
-    },
-    {
-        name: 'About Me',
-        to: 'about',
-        active: 'about'
-    },
-    {
-        name: info.initials,
+        name: "Portfolio",
         type: 'initials',
         to: '',
         active: 'home'
-    },
-    {
-        name: 'Portfolio',
-        to: 'portfolio',
-        active: 'portfolio'
     }
+    // ,
+    // {
+    //     name: 'Portfolio',
+    //     to: 'portfolio',
+    //     active: 'portfolio'
+    // }
 ]
 
 // This function is used to create a scroll offset to compensate for the navbar
 // when you click on the nav buttons to scroll down.
 const scrollWidthOffset = (el) => {
     const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
-    const yOffset = -80; 
-    window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
+    const yOffset = -80;
+    window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' });
 }
 
 
 export default function Navbar({ darkMode, handleClick, active, setActive }) {
 
     return (
-        <Box component={'nav'} width={'100%'} position={singlePage ? 'fixed' : 'relative'} className={darkMode? Style.dark : Style.light}>
+        <Box component={'nav'} width={'100%'} position={singlePage ? 'fixed' : 'relative'} className={darkMode ? Style.dark : Style.light}>
             <Box component={'ul'} display={'flex'} justifyContent={'center'} alignItems={'center'}
                 gap={{ xs: '2rem', md: '8rem' }}
                 textTransform={'lowercase'} fontSize={'1rem'}>
@@ -51,7 +71,7 @@ export default function Navbar({ darkMode, handleClick, active, setActive }) {
                     <Box key={index} component={'li'} className={(link.active === active && !link.type) && Style.active}
                         sx={{ borderImageSource: info.gradient }}>
                         <Link to={singlePage ? `#${link.to}` : `/${link.to}`}
-                        scroll={el => scrollWidthOffset(el)}
+                            scroll={el => scrollWidthOffset(el)}
                             smooth
                             onClick={() => setActive(link.active)} className={Style.link}>
                             {!link.type && <p style={{ padding: '0.5rem 0' }}>{link.name}</p>}
@@ -59,9 +79,9 @@ export default function Navbar({ darkMode, handleClick, active, setActive }) {
                         </Link>
                     </Box>
                 ))}
-                <li>
+                {/* <li>
                     <Toggler darkMode={darkMode} handleClick={handleClick} />
-                </li>
+                </li> */}
             </Box>
         </Box>
     )
